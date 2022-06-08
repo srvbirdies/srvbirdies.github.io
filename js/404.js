8909c3955a5c9f0aae124cc7848849cb0627f675
srvhsbadminton <98202788+srvhsbadminton@users.noreply.github.com>
2022-06-07 22:31:27 -0700

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -1,10 +1,1 @@
-var timer = 6;
-function myTimer () {
-  if (timer > 0) {
-    timer -= 1;
-    document.querySelector("p").innerHTML = "Redirecting you in: " + timer + " seconds";
-  } else {
-    window.location.href = "https://sites.google.com/view/srvhsbirdies/home";
-  }
-}
-setInterval(myTimer, 1000);
+window.location.href = "https://sites.google.com/view/srvhsbirdies/home";
